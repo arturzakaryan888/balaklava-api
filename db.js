3f3966c4ec11b9ff294fb91291032ac2bf0bf07c
Artur Zakaryan <arturzakaryan888@gmail.com>
2021-05-08 18:44:31 +0400

    change

--- db.js
+++ db.js
@@ -1,3 +1,4 @@
+/*
 const Pool = require('pg').Pool
 const pool = new Pool({
   user: 'postgres',
@@ -7,24 +8,15 @@ const pool = new Pool({
   database: 'balaklava'
   })
 
+*/
 
 const { Client } = require('pg');
 
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
-  user: 'postgres',
-  host: 'localhost',
-  password: 'root',
-  port: 5432,
 });
 
 client.connect();
 
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-});
 
 module.exports = client
