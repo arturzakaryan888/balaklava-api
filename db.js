297c7f6122e409451f8c69c63e68a975cc60f392
Artur Zakaryan <arturzakaryan888@gmail.com>
2021-05-08 18:34:05 +0400

    menak te ashxati

--- db.js
+++ db.js
@@ -1,11 +1,30 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
   user: 'postgres',
-  password: 'root',
   host: 'localhost',
+  password: 'root',
   port: 5432,
-  database: 'balaklava2033',
+  database: 'balaklava'
   })
 
 
-module.exports = pool
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  user: 'postgres',
+  host: 'localhost',
+  password: 'root',
+  port: 5432,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+});
+
+module.exports = client
--- init.sql
+++ init.sql
@@ -1,8 +1,84 @@
-CREATE TABLE Persons
-(
-PersonID int,
-LastName varchar(255),
-FirstName varchar(255),
-Address varchar(255),
-City varchar(255)
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 13.2
+-- Dumped by pg_dump version 13.2
+
+-- Started on 2021-05-08 18:29:08
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- TOC entry 201 (class 1259 OID 16397)
+-- Name: player; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.player (
+    id integer NOT NULL,
+    name character varying(255)
 );
+
+
+ALTER TABLE public.player OWNER TO postgres;
+
+--
+-- TOC entry 200 (class 1259 OID 16395)
+-- Name: player_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+ALTER TABLE public.player ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.player_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
+
+
+--
+-- TOC entry 2983 (class 0 OID 16397)
+-- Dependencies: 201
+-- Data for Name: player; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+
+
+--
+-- TOC entry 2989 (class 0 OID 0)
+-- Dependencies: 200
+-- Name: player_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.player_id_seq', 1, false);
+
+
+--
+-- TOC entry 2851 (class 2606 OID 16401)
+-- Name: player player_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.player
+    ADD CONSTRAINT player_pkey PRIMARY KEY (id);
+
+
+-- Completed on 2021-05-08 18:29:08
+
+--
+-- PostgreSQL database dump complete
+--
+
